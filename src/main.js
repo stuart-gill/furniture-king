daa74fec36cd8a866b1a295c806f5ac292996175
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-10 14:35:42 -0800

    id now works regardless of id duplication in API. Decided against Furniture object, just using the json parse objects

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { FurnitureSearch } from "./search";
-import { Furniture } from "./furniture.model";
 import $ from "jquery";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
@@ -15,57 +14,18 @@ $(document).ready(function() {
   promise.then(function(response) {
     let typeList = [];
     let body = JSON.parse(response);
-    // let furnitureArray = buildFurnitureFromJson(body);
-    // console.log(furnitureArray);
+
     for (let i = 0; i < body.body.data.length; i++) {
       body.body.data[i].id = i;
       if (!typeList.includes(body.body.data[i].type)) {
         typeList.push(body.body.data[i].type);
       }
-      // $("#name").append(`<li id= ${i}> ${body.body.data[i].name} </li>`);
     }
     typeList.forEach(printTypes);
 
     function printTypes(type) {
       $("#name").append(`<li class=furnitureType id= ${type}> ${type} </li>`);
     }
-
-    // function buildFurnitureFromJson(body) {
-    //   let tempArray = [];
-    //   for (let i = 0; i < body.body.data.length; i++) {
-    //     let id = i;
-    //     let name = body.body.data[i].name;
-    //     let imageUrl = body.body.data[i].imageUrl;
-    //     let description = body.body.data[i].description;
-    //     let colors = [];
-    //     for (let j = 0; j < body.body.data[i].colors.length; j++) {
-    //       colors.push(body.body.data[i].colors[j]);
-    //     }
-    //     let type = body.body.data[i].type;
-    //     let deliverable = body.body.data[i].deliverable;
-    //     let dimensions = [];
-    //     if (body.body.data[i].dimensions) {
-    //       dimensions.push(body.body.data[i].dimensions.length);
-    //       dimensions.push(body.body.data[i].dimensions.width);
-    //     }
-    //     let cost = body.body.data[i].cost;
-    //     let stock = body.body.data[i].stock;
-    //     let newItem = new Furniture(
-    //       id,
-    //       name,
-    //       imageUrl,
-    //       description,
-    //       colors,
-    //       type,
-    //       deliverable,
-    //       dimensions,
-    //       cost,
-    //       stock
-    //     );
-    //     tempArray.push(newItem);
-    //   }
-    //   return tempArray;
-    // }
 
     function printItems(type) {
       for (let i = 0; i < body.body.data.length; i++) {
@@ -87,8 +47,11 @@ $(document).ready(function() {
     }
 
     function printItemDetails(id) {
+      console.log(id);
+      id = parseInt(id);
       for (let i = 0; i < body.body.data.length; i++) {
         if (body.body.data[i].id === id) {
+          console.log("passed");
           $("#itemDetails").append(`<p><em>${body.body.data[i].name}</em></p>`);
           $("#itemDetails").append(`<p>${body.body.data[i].description}</p>`);
           $("#itemDetails").append(
@@ -117,14 +80,5 @@ $(document).ready(function() {
       let id = this.getAttribute("id");
       printItems(id);
     });
-
-    // $(".furnitureItem").click(function(event) {
-    //   event.preventDefault();
-    //   console.log("item clicked");
-    //   $("#itemDetails").empty();
-    //   let id = this.getAttribute("id");
-    //   printItemDetails(id);
-    //   console.log("item clicked");
-    // });
   });
 });
